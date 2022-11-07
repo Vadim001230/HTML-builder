d7a3b77da22bea634c3d44684d0b6d322462e599
Vadim Shershen <vadim.shershen.97@mail.ru>
2022-11-08 01:18:52 +0300

    feat: task 04 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const path = require('path');
+const fs = require('fs');
 
+const mainFolder = path.join(__dirname, 'files');
+const copyFolder = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+  fs.mkdir(copyFolder, { recursive: true }, err => {
+    if (err) throw err;
+  });
+
+  fs.readdir(copyFolder, {withFileTypes: true}, (err, files) => {
+    for (let file of files) {
+      fs.unlink(path.join(copyFolder, file.name), function () {
+      })
+    }
+    fs.readdir(mainFolder,{ withFileTypes: true }, (err, files) => {
+      if (err)
+        throw err;
+      else {
+        files.forEach(file => {
+          fs.copyFile(path.join(mainFolder, file.name), path.join(copyFolder, file.name), (err) => {
+            if (err) {
+              throw err;
+            }
+          });
+        })
+      }
+    })
+  })
+}
+
+copyDir();
